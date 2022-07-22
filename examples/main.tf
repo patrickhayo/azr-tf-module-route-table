829c55d133eb15840ac07c5e499c68b65d08578d
Patrick Hayo <patrick.hayo@email.de>
2022-07-22 14:30:19 +0200

    initial commit

--- examples/main.tf
+++ examples/main.tf
@@ -2,3 +2,26 @@ resource "azurerm_resource_group" "this" {
   name     = uuid()
   location = "westeurope"
 }
+
+module "rt" {
+  source                        = "./module"
+  name                          = "rt-example"
+  resource_group_name           = azurerm_resource_group.this.name
+  location                      = azurerm_resource_group.this.location
+  disable_bgp_route_propagation = true
+  routes = [
+    {
+      name                   = "default"
+      address_prefix         = "0.0.0.0/0"
+      next_hop_type          = "Virtual Appliance"
+      next_hop_in_ip_address = "192.168.255.1"
+    }
+  ]
+  subnets_to_associate = {
+    ("mySubneToAssociateName") = {
+      subscription_id      = "myVnetToAssociateSubscriptionId"
+      resource_group_name  = "myVnetToAssociateResourceGroupName"
+      virtual_network_name = "myVnetToAssociateName"
+    }
+  }
+}
